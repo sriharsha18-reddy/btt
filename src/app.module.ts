1217e62a5afd2941979165b7feb8b93d3a85cd2f
sriharsha18-reddy <nyalakondasriharsha123@gmail.com>
2024-12-14 01:15:00 +0530

    after this changing the user totaly this one was working but missing something

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest'), // Replace with your MongoDB URI
+    UserModule,
+  ],  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -6,9 +6,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from '../schemas/user.schema';
 
 @Module({
-  // imports: [
-    // MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-  // ],
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
